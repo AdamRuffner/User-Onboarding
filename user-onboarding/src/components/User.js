a0efac8fe0b03593d5fa6aa0c7ed460ce93ca8f4
Adam <ruffneradam@aol.com>
2020-09-17 10:47:54 -0400

    added name functionality

--- user-onboarding/src/components/User.js
+++ user-onboarding/src/components/User.js
@@ -8,11 +8,8 @@ export default function User({ details }) {
     return (
         <div className= 'user container'>
             <h3>{details.first_name}</h3>
+            <h3>{details.name}</h3>
             <p>email: {details.email}</p>
-           
-        
-
-
         </div>
     )
 }
